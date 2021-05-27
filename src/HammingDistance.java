fe067d853efc359e145ea6834692d1348051c55f
pei Wang <44791528+wangpei72@users.noreply.github.com>
2021-05-27 20:08:32 +0800

    lc461 汉明距离

---
+++ src/HammingDistance.java
@@ -0,0 +1,10 @@
+public class HammingDistance {
+    public static int hd(int x, int y){
+        int s = x ^ y, ret = 0;
+        while (s != 0) {
+            ret += s & 1;
+            s >>= 1;
+        }
+        return ret;
+    }
+}
